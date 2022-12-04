8d3dbbc6c9ad041e3d3c4952c67a567b92e4030c
slepishh <83390207+slepishh@users.noreply.github.com>
2022-12-04 15:04:07 +0300

    2
    2

--- gulp/tasks/fonts.js
+++
@@ -1,5 +0,0 @@
-export const fonts = () => {
-    return app.gulp.src(`${app.path.srcFolder}/fonts/**`, {})
-        .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/**`))
-        .pipe(app.gulp.dest(`${app.path.build.fonts}`));
-}
